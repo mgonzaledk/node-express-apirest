76ac0ad0709932753f49fddf516cd1ea3e19b781
M. A. Gonzalez <mgonzaledk@alumnos.unex.es>
2018-12-27 17:29:20 +0100

    Auth JWT.

---
+++ Route/Auth.js
@@ -0,0 +1,29 @@
+var jwt = require('express-jwt')
+var key = require('../Config/Key')
+
+function GetToken(request) {
+    if(request.headers.authorization) {
+        const split = req.headers.authorization.split(' ')
+
+        if(split[0] === 'Token' || split[0] === 'Bearer') {
+            return split[1]
+        }
+    }
+
+    return null
+}
+
+module.exports = {
+    required: jwt({
+        secret: key.private_keyphrase,
+        userProperty: 'payload',
+        getToken: GetToken
+    }),
+
+    optional: jwt({
+        secret: key.private_keyphrase,
+        userProperty: 'payload',
+        credentialsRequired: false,
+        getToken: GetToken
+    })
+}
